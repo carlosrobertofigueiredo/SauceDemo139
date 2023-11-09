1f7081bc986fff220a06f1015dfecbccb216c209
carlosrobertofigueiredo <carlosfigueiredo@souunisuam.com.br>
2023-11-08 22:50:10 -0300

    Atualização de código.
    Realizado algumas configurações no CartPages, InventoryPages e selectProductPO.cs

---
+++ Pages/InventoryPages.cs
@@ -0,0 +1,15 @@
+using OpenQA.Selenium;
+
+namespace Pages
+{
+    public class InventoryPage : CommonPage
+    {
+        // Mapeamento dos Elementos
+
+        // Construtor
+        public InventoryPage(IWebDriver driver) : base(driver){}
+
+        // Ações
+
+    }
+}
